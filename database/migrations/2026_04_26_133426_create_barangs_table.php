39de2c85f9fc0fc94e2dc68d231470552b8a0630
ghatfan <fanghat99@gmail.com>
2026-04-26 20:38:21 +0700

    create table

--- database/migrations/2026_04_26_133426_create_barangs_table.php
+++ database/migrations/2026_04_26_133426_create_barangs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_barang');
+            $table->string('kode_barang');
+            $table->integer('harga_barang');
             $table->timestamps();
         });
     }
